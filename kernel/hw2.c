4c680bdb2785189b84f41eab2bc3b7d52f912e1a
taligrayzel <tali.grayzel@campus.technion.ac.il>
2023-05-26 23:09:39 +0300

    my syscalls

--- kernel/hw2.c
+++ kernel/hw2.c
@@ -7,6 +7,13 @@ asmlinkage long sys_hello(void) {
 
 asmlinkage long sys_set_weight(int weight) { //TODO: Tali
   // TODO: add implementation
+  if( weight < 0 )
+  {
+    return -EINVAL; 
+  }
+  struct task_struct *task = current;
+  task->weight = weight;
+  return 0; 
 }
 
 asmlinkage long sys_get_weight(void) { //TODO: Hen
@@ -15,6 +22,15 @@ asmlinkage long sys_get_weight(void) { //TODO: Hen
 
 asmlinkage long sys_get_ancestor_sum(void) { //TODO: Tali
   // TODO: add implementation
+  int sum_weight = 0;
+  struct task_struct *task = current;
+  while( task->pid != 0)
+  {
+    sum_weight = sum_weight + task->weight ;
+    task = task->parent ; 
+  }
+  sum_weight = sum_weight + task->weight ;
+  return sum_weight;
 }
 
 asmlinkage long sys_get_heaviest_descendant(void) { //TODO: Hen
