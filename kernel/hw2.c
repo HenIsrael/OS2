93fd9a8297c33fc5d6b824b772a6d98bb5cde46e
taligrayzel <tali.grayzel@campus.technion.ac.il>
2023-05-27 13:42:15 +0300

    my func

--- kernel/hw2.c
+++ kernel/hw2.c
@@ -18,6 +18,8 @@ asmlinkage long sys_set_weight(int weight) { //TODO: Tali
 
 asmlinkage long sys_get_weight(void) { //TODO: Hen
   // TODO: add implementation
+  struct task_struct *task = get_current();
+  return task->weight;
 }
 
 asmlinkage long sys_get_ancestor_sum(void) { //TODO: Tali
@@ -33,6 +35,39 @@ asmlinkage long sys_get_ancestor_sum(void) { //TODO: Tali
   return sum_weight;
 }
 
-asmlinkage long sys_get_heaviest_descendant(void) { //TODO: Hen
+asmlinkage pid_t sys_get_heaviest_descendant(void)
+{ //TODO: Hen
   // TODO: add implementation
+  struct task_struct* task = get_current();
+  pid_t maxPID = task->pid;
+  long maxWeight = task->weight;
+
+  struct task_struct* child;
+  struct list_head* list;
+
+  // Check if the current task has children
+  if (list_empty(&task->children))
+    return -ECHILD;
+
+  // Traverse the child process list
+  list_for_each(list, &task->children) {
+
+    child = list_entry(list, struct task_struct, sibling);
+
+    // Recursively find the task with the biggest weight among descendants
+    pid_t childMaxWeightPID = sys_get_heaviest_descendant();
+
+    // Compare weights and choose the task with the biggest weight
+    if (child->weight > maxWeight) {
+      maxWeight = child->weight;
+      maxPID = child->pid;
+    }
+
+    // If weights are the same, compare pids and choose the task with the biggest pid
+    else if (child->weight == maxWeight && childMaxWeightPID > maxPID) {
+       maxPID = childMaxWeightPID;
+     
+    }
+  }
+  return maxPID;
 }
